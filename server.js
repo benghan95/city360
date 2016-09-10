35b8212d6eada4b18b3cc9a976228fd8382339be
benghan95 <martin_ng@live.com>
2016-09-10 11:49:37 +0800

    server bug fix for syntax error

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ function handleError(res, reason, message, code) {
  */
 
 app.get("/projects", function(req, res){
-  db.collection(PROJECTS_COLLECTION).find({}).toArray(function(err.docs){
+  db.collection(PROJECTS_COLLECTION).find({}).toArray(function(err, docs){
     if(err){
       handleError(res, err.message, "Failed to retrieve projects.");
     } else{
